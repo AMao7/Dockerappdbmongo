9364547ea7e86dd07f8898bef1c94ca185660dd8
admim <admin@android-4be2246982f81526.home>
2020-09-21 14:10:10 +0100

    first commit

---
+++ app/app.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var port = 3000
+var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
+var url = 'mongodb://localhost:27017/myapp';
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+
+
+
+app.get('/', function(req, res){
+    res.render('index');
+ });  // This is for the home page
+
+
+ app.get('/addname', function(req, res){
+    res.render('addname');
+ });
+ // This is the saved details page
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://mongodb:27017/myapp1");
+
+var nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    emailAddress: String,
+});
+var User = mongoose.model("User", nameSchema);
+
+
+
+// This creates or uses an exisitng db and adds the user details as strings
+
+
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+    .then(item => {
+        res.render('saved');
+        })
+        .catch(err => {
+        res.status(400).send("unable to save to database");
+        });
+       });
+
+// This will redirect you the saved page after successfully adding your details
+
+app.get("/getdetails", function (req, res) {
+    User.find({}, function(err, users) {
+    if (err) throw err;
+    // object of all the users
+    res.render('hello',{users:users});
+})
+});
+
+// This is the database page
+
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
